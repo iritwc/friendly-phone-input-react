b8f6693ec835e23bb6354d925c31f5bca29bd416
Anona <15170559+iritwc@users.noreply.github.com>
2023-07-09 18:37:59 +0000

    Testing: add cutting edge unit test

--- src/component/phoneInput.test.js
+++ src/component/phoneInput.test.js
@@ -1,10 +1,10 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, keyboard } from '@testing-library/react';
 import PhoneInput from './phoneInput';
+// import { keyboard } from '@testing-library/user-event';
 
 const setup = async () => {
   const utils = render(<PhoneInput />);
-  const input = await screen.getByLabelText('(123) 456-7890');
-  console.log(input.value);
+  const input = await screen.getByRole('textbox', {name: "(123) 456-7890"});
   return {
     input,
     ...utils,
@@ -20,20 +20,44 @@ test('It should not allow letters to be inputted', async () => {
   expect(input.value).toBe('') //empty after
 });
 
-test('It should keep first 3 digits non formatted', async () => {
+test('It should fromat for size<=3', async () => {
   const {input} = await setup()
   fireEvent.change(input, {target: {value: '123'}})
   expect(input.value).toBe('123');
 });
 
-test('It should format input of size > 3 && size<=6', async () => {
+test('It should format for size > 3 && size<=6', async () => {
   const {input} = await setup()
   fireEvent.change(input, {target: {value: '123456'}})
   expect(input.value).toBe('(123) 456');
 });
 
-test('It should format input of size > 6', async () => {
+test('It should format for size > 6', async () => {
   const {input} = await setup()
   fireEvent.change(input, {target: {value: '1234567890'}})
   expect(input.value).toBe('(123) 456-7890');
+});
+
+test('It should reposition the caret and keep format rule on Insert', async () => {
+  const {input} = await setup()
+  fireEvent.change(input, {target: {value: '124'}});
+  expect(input.value).toBe('124') // need to make a change so React registers "" as a change
+  expect(input.selectionStart).toBe(3);
+  // Simulate a 'Backspace' keydown
+  fireEvent.change(input, {target: {value: input.value, selectionStart: input.selectionStart-1}});
+  expect(input.selectionStart).toBe(2);
+  // fireEvent.keyDown(input, {key: 'ArrowLeft', code: 'ArrowLeft', keyCode: 37});
+  fireEvent.keyDown(input, {key: 3, code: 'Digit3', keyCode: 51});
+  fireEvent.change(input, {target: {value: '1234', selectionStart: 3}});
+  expect(input.value).toBe('(123) 4');
+  expect(input.selectionStart).toBe(4);
+});
+
+test('It should reposition the caret and keep format rule on Delete', async () => {
+  const {input} = await setup()
+  fireEvent.change(input, {target: {value: '12345567'}});
+  expect(input.value).toBe('(123) 455-67');
+  fireEvent.change(input, {target: {value: '(123) 45-67', selectionStart:9 }});
+  expect(input.value).toBe('(123) 456-7');
+  expect(input.selectionStart).toBe(8);
 });
